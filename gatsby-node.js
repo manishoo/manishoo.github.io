3a9b369cef6142abda6b94809d7d82222f362d45
Mani Shooshtari <mani@shooshtari.com>
2021-11-29 12:25:18 +0300

    better build folder handling

--- gatsby-node.js
+++ gatsby-node.js
@@ -102,3 +102,15 @@ exports.onCreateWebpackConfig = ({ stage, loaders, actions }) => {
     },
   });
 };
+
+exports.onPreInit = () => {
+  if (process.argv[2] === 'build') {
+    fs.rmdirSync(path.join(__dirname, 'docs'), { recursive: true });
+    fs.renameSync(path.join(__dirname, 'public'), path.join(__dirname, 'public_dev'));
+  }
+};
+
+exports.onPostBuild = () => {
+  fs.renameSync(path.join(__dirname, 'public'), path.join(__dirname, 'docs'));
+  fs.renameSync(path.join(__dirname, 'public_dev'), path.join(__dirname, 'public'));
+};
